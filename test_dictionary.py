5fef61731f6ec67e25923871103b15bb4121170e
Divyanshu Tomar <divyanshutomar27@gmail.com>
2024-09-13 21:42:32 +0530

    test file added

---
+++ test_dictionary.py
@@ -0,0 +1,9 @@
+def test_dict_equality():
+    a = dict(one=1, two=2, three=3)
+    b = {'one': 1, 'two': 2, 'three': 3}
+    c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+    d = dict([('two', 2), ('one', 1), ('three', 3)])
+    e = dict({'three': 3, 'one': 1, 'two': 2})
+    f = dict({'one': 1, 'three': 3}, two=2)
+
+    assert a == b == c == d == e == f
