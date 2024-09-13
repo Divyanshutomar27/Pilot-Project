32bfbc24eb50d31845a11ed90cfc890654a178ce
Divyanshu Tomar <divyanshutomar27@gmail.com>
2024-09-13 15:16:20 +0530

    new changes

--- dictionary.py
+++ dictionary.py
@@ -8,16 +8,16 @@ print(list(tel))
 
 ###########################################
 
-## construct a dictionary using dict() constructor ##
+# construct a dictionary using dict() constructor ##
 
-## below all examples  return a dictionary 
+# below all examples  return a dictionary 
 
-# a = dict(one=1, two=2, three=3)
-# b = {'one': 1, 'two': 2, 'three': 3}
-# c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
-# d = dict([('two', 2), ('one', 1), ('three', 3)])
-# e = dict({'three': 3, 'one': 1, 'two': 2})
-# f = dict({'one': 1, 'three': 3}, two=2)
+a = dict(one=1, two=2, three=3)
+b = {'one': 1, 'two': 2, 'three': 3}
+c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+d = dict([('two', 2), ('one', 1), ('three', 3)])
+e = dict({'three': 3, 'one': 1, 'two': 2})
+f = dict({'one': 1, 'three': 3}, two=2)
 
 a = dict(one=1, two=2, three=3)
 
