51b0e0968d9dfa983256cd58941e93d1971e54aa
Divyanshu Tomar <divyanshutomar27@gmail.com>
2024-09-13 15:14:52 +0530

    new commit

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+tel = {'jack': 4098, 'sape': 4139}
+
+tel['stree2'] = 5000
+
+print(tel['stree2'])
+
+print(list(tel))
+
+###########################################
+
+## construct a dictionary using dict() constructor ##
+
+## below all examples  return a dictionary 
+
+# a = dict(one=1, two=2, three=3)
+# b = {'one': 1, 'two': 2, 'three': 3}
+# c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+# d = dict([('two', 2), ('one', 1), ('three', 3)])
+# e = dict({'three': 3, 'one': 1, 'two': 2})
+# f = dict({'one': 1, 'three': 3}, two=2)
+
+a = dict(one=1, two=2, three=3)
+
+b = {'one': 1, 'two': 2, 'three': 3}
+
+e = dict({'three': 3, 'one': 1, 'two': 2})
+
+for  b in e:
+    print(b)
+
